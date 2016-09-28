a9ae3d020cf5f2898f13aff2a1846f973cfc46be
matbecker <7m4tt3r7@gmail.com>
2016-09-28 00:23:05 -0400

    Scene set up, modelled and built
    Models, textures, materials and sounds have been imported. Scene has
    been modelled and is collidable. Still needs lighting and a nav mesh.

---
+++ Crazy Mad Office Dude/Assets/Editor/BatchRename.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BatchRename : ScriptableWizard {
+
+	public string baseName = "MyObject_";
+	public int startNumber = 0;
+	public int increment = 1;
+
+	[MenuItem("Edit/Batch Rename...")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard("Batch Rename", typeof(BatchRename), "Rename");
+	}
+	//called when the window first appears 
+	void OnEnable()
+	{
+		UpdateSelectionHelper();
+	}
+	//called when the selection changes in the scene
+	void OnSelectionChange()
+	{
+		UpdateSelectionHelper();
+	}
+	//update selection counter
+	void UpdateSelectionHelper()
+	{
+		//sets the help text of the wizard
+		helpString = "";
+
+		//tells you how many objects are selected
+		if (Selection.objects != null)
+			helpString = "Number of objects selected: " + Selection.objects.Length;
+
+	}
+	//rename
+	void OnWizardCreate()
+	{
+		//if no objects are selected than return
+		if (Selection.objects == null)
+			return;
+
+		int postFix = startNumber;
+
+		//iterate through selected objects
+		foreach (Object o in Selection.objects)
+		{
+			o.name = baseName + postFix;
+
+			postFix += increment;
+		}
+	}
+
+}
---
+++ Crazy Mad Office Dude/Assets/Scripts/FBX_Import.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class FBX_Import : AssetPostprocessor {
+
+	public const float scale = 1.0f;
+
+	//called every time the a model is imported into unity
+	void OnPreprocessModel()
+	{
+		ModelImporter importer = assetImporter as ModelImporter;
+		importer.globalScale = scale;
+	}
+}
